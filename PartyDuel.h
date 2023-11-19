b9a347d9dfdba12579768204558445a3a38b1b69
Ziad Oweis <ziadoweis@gmail.com>
2023-11-19 12:24:54 -0500

    Fixed IRace.h errors

---
+++ PartyDuel.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CharacterCreator.h"
+
+// Professions
+#include "Professions/Archer.h"
+#include "Professions/Assassin.h"
+#include "Professions/Mage.h"
+#include "Professions/Warrior.h"
+
+// Races
+#include "Races/DarkElf.h"
+#include "Races/Khajit.h"
+#include "Races/Nord.h"
+#include "Races/Orc.h"
+
+#include <memory>
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::cout << "Justin is eating ziad's ass" << std::endl;
+}
